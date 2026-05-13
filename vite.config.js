5f3d6b45390a198e4dcda5898dbeb37e8d73d09f
阿杰 <506907958@qq.com>
2026-05-14 00:04:29 +0800

    添加 自动导入功能

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,45 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import { bunny } from "laravel-vite-plugin/fonts";
-import unoCss from "unocss/vite";
-import pug from "vite-plugin-pug";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import { bunny } from 'laravel-vite-plugin/fonts';
+import unoCss from 'unocss/vite';
+import pug from 'vite-plugin-pug';
+import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 export default defineConfig({
-    plugins: [
-        unoCss(),
-        pug(),
-        vue(),
-        laravel({
-            input: ["resources/styles/main.css", "resources/js/main.js"],
-            refresh: true,
-            fonts: [
-                bunny("Instrument Sans", {
-                    weights: [400, 500, 600],
-                }),
-            ],
+  plugins: [
+    unoCss(),
+    pug(),
+    vue(),
+    AutoImport({
+      imports: ['vue', 'vue-router', 'pinia'],
+      resolvers: [ElementPlusResolver()],
+      dts: true,
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+      dts: true,
+    }),
+    laravel({
+      input: ['resources/styles/main.css', 'resources/js/main.js'],
+      refresh: true,
+      fonts: [
+        bunny('Instrument Sans', {
+          weights: [400, 500, 600],
         }),
-    ],
-    server: {
-        watch: {
-            ignored: ["**/storage/framework/views/**"],
-        },
+      ],
+    }),
+  ],
+  server: {
+    watch: {
+      ignored: ['**/storage/framework/views/**'],
     },
-    resolve: {
-        alias: {
-            "@": "/resources/js",
-        },
+  },
+  resolve: {
+    alias: {
+      '@': '/resources/js',
     },
+  },
 });
